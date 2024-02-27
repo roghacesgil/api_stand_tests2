137c4f84ae9cfa8d2f94e3d5d471276458fe3f2e
Rogelio Triple Ten <rogeliotripleten@MacBook-Air-de-Rogelio-2.local>
2024-02-26 21:51:50 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-26d9d530-aa71-4cd0-8e24-58d2a4f6e55b.containerhub.tripleten-services.com/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
